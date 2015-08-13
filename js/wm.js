f186e41c54bb0e655763957e933f636e1221cfef
Frederick <frederick.jaime@mry.com>
2015-08-13 14:06:10 -0400

    initial setup for wegmens

---
+++ js/wm.js
@@ -0,0 +1,105 @@
+/*!
+ * Wegmens
+ * Fredie J. Aug.13.15:Thursday v0.1
+ */
+ /*
+*/
+
+   
+
+var wegmens = (function ($) {
+  // Variables - Variables available throughout the scope of this object
+  // -------------------------------------------------------------------
+
+var _scrollLoc_,
+    _windowHeight_,
+    _windowWidth_;
+    
+
+
+
+
+    
+  // Init - Anything you want to happen onLoad (usually event bindings)
+  // -------------------------------------------------------------------
+  var init = function () {
+     
+    
+
+
+
+
+
+    _windowWidth_ = $( window ).width(); 
+    _windowHeight_ = $( window ).height(); 
+    
+    
+   
+
+
+    
+
+
+    $( window ).resize(function(){
+      
+       _windowWidth_ = $( window ).width(); 
+
+
+
+    });
+
+ 
+
+
+/* WINDOW SCROLL : BEGIN */
+
+	$( window ).scroll(function(){
+
+		_scrollLoc_ = $( window ).scrollTop();
+
+      
+           
+        
+      
+  
+	});
+
+	
+
+
+};
+
+
+
+
+  // FUNCTIONS
+  // ===================================================================
+  // ===================================================================
+  // ===================================================================
+
+
+
+
+  // CLICKING
+  // ===================================================================
+    /*------------------------*/
+  
+
+
+
+
+  // CLEANUP
+  // ===================================================================
+
+  // Return - Which variables and objects to make available publicly
+  // -------------------------------------------------------------------
+  return {
+    init              : init
+  };
+})(jQuery);
+
+
+$(document).ready(function () {    wegmens.init();    });
+
+  
+
